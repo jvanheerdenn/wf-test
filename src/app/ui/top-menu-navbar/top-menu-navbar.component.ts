077bbc32b9690636798aef0f703278e8876930f6
X485546 <jvanheerden@vectoritcgroup.com>
2022-09-04 17:08:39 +0200

    Feat: avoid sub in top-menu-bar + close offcanvas

--- src/app/ui/top-menu-navbar/top-menu-navbar.component.ts
+++ src/app/ui/top-menu-navbar/top-menu-navbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TopNavbarLinks } from '../models/top-menu-navbar.model';
 
@@ -8,13 +8,10 @@ import { TopNavbarLinks } from '../models/top-menu-navbar.model';
   styleUrls: ['./top-menu-navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TopMenuNavbarComponent implements OnInit {
+export class TopMenuNavbarComponent {
   @Input() links$!: Observable<TopNavbarLinks[]>;
   @Output() urlLink = new EventEmitter<string>();
 
-  ngOnInit(): void {
-    this.links$.subscribe((val) => console.log(val));
-  }
   onMenuClick({ navigation }: TopNavbarLinks) {
     this.urlLink.emit(navigation);
   }
